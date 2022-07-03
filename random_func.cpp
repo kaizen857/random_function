acaed264a3176cdad1c48b26fd15480245cc6e5f
kaizen857 <54053369+kaizen857@users.noreply.github.com>
2022-07-03 12:20:45 +0800

    Update random_func.cpp

--- random_func.cpp
+++ random_func.cpp
@@ -1,15 +1,3 @@
-/*
-给所有打开这个文件的人的一句话：
-如果你是一个不会编程的人的话请不要修改以下代码，这个项目在这台电脑里不会有任何备份
-如果你是一个会编c++的人的话：
-如果你有时间，请继续帮忙改进这个项目，这个项目因为开始的时间很晚，开的时候已经是快期末了，所以有很多地方我还没完善
-比如说加图形化以及还有将std::uniform_int_distribution改为std::discrete_distribution之类的。
-然后如果可以的话，请将所有的修改提交到我的github仓库里面
-仓库地址：
-
-*/
-
-
 #include<iostream>
 #include<conio.h>
 #include<windows.h>
